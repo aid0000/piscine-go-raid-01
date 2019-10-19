29afcc8b5f8db970f7c86eccd3892fe25c324c28
ymakovet <makovetskaya.y@gmail.com>
2019-10-19 15:17:33 +0600

    fixed2

--- raid1a.go
+++ raid1a.go
@@ -2,10 +2,10 @@ package student
 
 import "github.com/01-edu/z01"
 
-func Raid1a(x,y int) {	
+func Raid1a(x, y int) {	
 	for y1 := y; y1 > 0; y1-- {
 		for x1 := x; x1 > 0; x1-- {
-			if (y1 == y && x1 == x) || (y1 == 1 && x1 == 1) || (y1 == y && x1 == 1) || (y1 == 1 && x1 == x)  {
+			if (y1 == y && x1 == x) || (y1 == 1 && x1 == 1) || (y1 == y && x1 == 1) || (y1 == 1 && x1 == x) {
 				z01.PrintRune('o')
 			} else if x1 == x || x1 == 1 {
 				z01.PrintRune('|')
