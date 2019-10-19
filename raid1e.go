615231192b50f1d476ca4024d45387d180016c82
ymakovet <makovetskaya.y@gmail.com>
2019-10-19 17:41:23 +0600

    e

--- raid1e.go
+++ raid1e.go
@@ -8,13 +8,13 @@ func Raid1e(x, y int) {
 	}
 	for y1 := 0; y1 < y; y1++ {
 		for x1 := 0; x1 < x; x1++ {
-			if (y1 == 0 && x1 == 0) || (y != 1 && y1 == y - 1 && x1 == x - 1 && x != 1) {
+			if (y1 == 0 && x1 == 0) || (y != 1 && y1 == y-1 && x1 == x-1 && x != 1) {
 				z01.PrintRune('A')
-			} else if (y1 == 0 && x1 == x - 1) || (y1 == y - 1 && x1 == 0) {
+			} else if (y1 == 0 && x1 == x-1) || (y1 == y-1 && x1 == 0) {
 				z01.PrintRune('C')
-			} else if x1 == x - 1 || x1 == 0 {
+			} else if x1 == x-1 || x1 == 0 {
 				z01.PrintRune('B')
-			} else if y1 == y - 1 || y1 == 0 {
+			} else if y1 == y-1 || y1 == 0 {
 				z01.PrintRune('B')
 			} else {
 				z01.PrintRune(' ')
