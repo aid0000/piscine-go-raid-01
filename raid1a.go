c8c2cb13d534bcf42c8f40ff158b95e0bbc8d1d5
ymakovet <makovetskaya.y@gmail.com>
2019-10-19 15:18:21 +0600

    fixed2

--- raid1a.go
+++ raid1a.go
@@ -2,7 +2,7 @@ package student
 
 import "github.com/01-edu/z01"
 
-func Raid1a(x, y int) {	
+func Raid1a(x, y int) {
 	for y1 := y; y1 > 0; y1-- {
 		for x1 := x; x1 > 0; x1-- {
 			if (y1 == y && x1 == x) || (y1 == 1 && x1 == 1) || (y1 == y && x1 == 1) || (y1 == 1 && x1 == x) {
