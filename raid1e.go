214a21d749c1551ed85b7f2f885d52e9f4303403
ymakovet <makovetskaya.y@gmail.com>
2019-10-19 17:39:52 +0600

    e

---
+++ raid1e.go
@@ -0,0 +1,25 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Raid1e(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	}
+	for y1 := 0; y1 < y; y1++ {
+		for x1 := 0; x1 < x; x1++ {
+			if (y1 == 0 && x1 == 0) || (y != 1 && y1 == y - 1 && x1 == x - 1 && x != 1) {
+				z01.PrintRune('A')
+			} else if (y1 == 0 && x1 == x - 1) || (y1 == y - 1 && x1 == 0) {
+				z01.PrintRune('C')
+			} else if x1 == x - 1 || x1 == 0 {
+				z01.PrintRune('B')
+			} else if y1 == y - 1 || y1 == 0 {
+				z01.PrintRune('B')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune(10)
+	}
+}
